8f6d94d0a540cf81c76a718ffa381dd3806239d0
Rick Troth <trothr@gmail.com>
2023-02-28 17:06:53 -0500

    2.0.36

--- cmsmake.h
+++ cmsmake.h
@@ -1,12 +1,12 @@
 /*                                                                    *
  *                                                                    *
  *        Name: cmsmake.h (C language header file for CMS Make)       *
- *        Date: 2008-Jan-26                                           *
+ *        Date: 2008-Jan-26 and following                             *
  *              This file is part of the CMS Make package.            *
  *                                                                    *
  *                                                                    *
  *                                                                    */
 
-#define  MAKE_VERSION  "2.0.34"
+#define  MAKE_VERSION  "2.0.36"
 
 
